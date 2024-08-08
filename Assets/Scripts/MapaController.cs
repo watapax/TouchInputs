5567cbabb3d4e8a1971baf4899d8ca92dd3faa9b
watapax <watapax@gmail.com>
2024-08-07 22:57:10 -0400

    paneo mapa

--- Assets/Scripts/MapaController.cs
+++ Assets/Scripts/MapaController.cs
@@ -4,19 +4,37 @@ using UnityEngine;
 using Cinemachine;
 public class MapaController : MonoBehaviour
 {
-    public bool activarMapController;
+    public bool puedePanear, puedeRotar, puedeHacerZoom;
     public CinemachineVirtualCamera virtualCamera;
     public Transform cameraTarget;
-    public float sensibilidad = 0.03f;
 
     Touch touch0, touch1;
-    Vector2 promedio;
+    
+    // para el paneo
+    public float sensibilidadPaneo = 0.5f;
+    public float sensibilidadMinimaPaneo = 0.1f;
+    float porcentajeDeZoom;
+    float maxSensibility;
     Vector3 newPos;
+
+    // para la rotacion
+    public float sensibilidadRotacion;
+    Vector2 previousTouchPosition1;
+    Vector2 previousTouchPosition2;
+
+    // para el zoom
+    Vector2 currentTouchPosition1; 
+    Vector2 currentTouchPosition2;
+    float initialDistance;
+    float initialCameraFOV;
+    public float minZoom, maxZoom;
+    
 
     private void Start()
     {
-        newPos = cameraTarget.position;
         SetCameraTarget(cameraTarget);
+        maxSensibility = sensibilidadPaneo;
+        porcentajeDeZoom = (virtualCamera.m_Lens.FieldOfView - minZoom) / (maxZoom - minZoom);
     }
 
     public void SetCameraTarget(Transform _transformTarget)
@@ -27,34 +45,96 @@ public class MapaController : MonoBehaviour
 
     private void Update()
     {
-        if (!activarMapController) return;
 
         if(Input.touchCount == 2)
+        { 
+            touch0 = Input.GetTouch(0);
+            touch1 = Input.GetTouch(1);
+            RotarMapa();
+            PinchAndZoom();
+        }
+        if(Input.touchCount == 1 )
         {
+            touch0 = Input.GetTouch(0);
             CameraPaning();
         }
 
-        cameraTarget.position = Vector3.Lerp(cameraTarget.position, newPos, 10 * Time.deltaTime);
     }
+
+    // PANEO
 
     void CameraPaning()
     {
-        touch0 = Input.GetTouch(0);
-        touch1 = Input.GetTouch(1);
+        if (!puedePanear) return;
 
-        promedio.x = (touch0.deltaPosition.x + touch1.deltaPosition.x) / 2;
-        promedio.y = (touch0.deltaPosition.y + touch1.deltaPosition.y) / 2;
+        sensibilidadPaneo = maxSensibility * porcentajeDeZoom;
+        sensibilidadPaneo = Mathf.Clamp(sensibilidadPaneo, sensibilidadMinimaPaneo, maxSensibility);
 
-        newPos = cameraTarget.position;
-        newPos.x -= promedio.x * sensibilidad;
-        newPos.z -= promedio.y * sensibilidad;
+        newPos.Set(touch0.deltaPosition.x * -1, 0, touch0.deltaPosition.y * -1);
+        newPos *= sensibilidadPaneo;
 
-
+        if (touch0.phase == TouchPhase.Moved)
+        {
+            cameraTarget.Translate(newPos * Time.deltaTime, Space.Self);
+        }
 
     }
 
+
+    // ZOOM
+
     void PinchAndZoom()
     {
+        if (!puedeHacerZoom) return;
+
+        float currentDistance = Vector2.Distance(touch0.position, touch1.position);
+
+        if (touch0.phase == TouchPhase.Began || touch1.phase == TouchPhase.Began)
+        {
+            initialDistance = currentDistance;
+            initialCameraFOV = virtualCamera.m_Lens.FieldOfView;
+        }
+        else
+        {
+            float scaleFactor =  initialDistance / currentDistance;
+            virtualCamera.m_Lens.FieldOfView = initialCameraFOV * scaleFactor;
+            virtualCamera.m_Lens.FieldOfView = Mathf.Clamp(virtualCamera.m_Lens.FieldOfView, minZoom, maxZoom);
+            float currentFov = virtualCamera.m_Lens.FieldOfView;
+            porcentajeDeZoom = (currentFov - minZoom) / (maxZoom - minZoom);
+        }
+
+    }
+
+
+    // ROTAR
+
+    void RotarMapa()
+    {
+        if (!puedeRotar) return;
+
+        currentTouchPosition1 = touch0.position;
+        currentTouchPosition2 = touch1.position;
+
+        if(touch0.phase == TouchPhase.Began || touch1.phase == TouchPhase.Began)
+        {
+            previousTouchPosition1 = currentTouchPosition1;
+            previousTouchPosition2 = currentTouchPosition2;
+        }
+        else
+        {
+            Vector2 previousDirection = previousTouchPosition2 - previousTouchPosition1;
+            Vector2 currentDirection = currentTouchPosition2 - currentTouchPosition1;
+
+            float previousAngle = Mathf.Atan2(previousDirection.y, previousDirection.x) * Mathf.Rad2Deg;
+            float currentAngle = Mathf.Atan2(currentDirection.y, currentDirection.x) * Mathf.Rad2Deg;
+
+            float angleDelta = (currentAngle - previousAngle) * sensibilidadRotacion;
+
+            cameraTarget.Rotate(0, angleDelta, 0);
+
+            previousTouchPosition1 = currentTouchPosition1;
+            previousTouchPosition2 = currentTouchPosition2;
+        }
 
     }
     
